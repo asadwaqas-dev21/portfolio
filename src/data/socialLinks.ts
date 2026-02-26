9ce86aa7d03b8ac77fda3fec970bb5a57d7de1b8
asadwaqas-dev21 <asadkambo2021@gmail.com>
2026-02-26 12:02:17 +0500

    change social media links

--- src/data/socialLinks.ts
+++ src/data/socialLinks.ts
@@ -8,19 +8,19 @@ export interface SocialLink {
 export const heroSocialLinks: SocialLink[] = [
   {
     name: "LinkedIn",
-    url: "https://www.linkedin.com/in/mazanaqvi/",
+    url: "https://www.linkedin.com/in/asad-waqas-87268a34a/",
     icon: "fab fa-linkedin-in",
     cssClass: "linkedin",
   },
   {
     name: "GitHub",
-    url: "https://github.com/mazanaqvi",
+    url: "https://github.com/asadwaqas-dev21",
     icon: "fab fa-github",
     cssClass: "github",
   },
   {
     name: "Stack Overflow",
-    url: "https://stackoverflow.com/users/13111347/ali-hamza",
+    url: "https://stackoverflow.com/users/23524200/asad-waqas",
     icon: "fab fa-stack-overflow",
     cssClass: "stackoverflow",
   },
@@ -29,31 +29,31 @@ export const heroSocialLinks: SocialLink[] = [
 export const contactSocialLinks: SocialLink[] = [
   {
     name: "Facebook",
-    url: "https://www.facebook.com/mazaanaqvi/",
+    url: "https://www.facebook.com/asadwaqas3134/",
     icon: "fab fa-facebook-f",
     cssClass: "facebook",
   },
   {
     name: "Twitter",
-    url: "https://twitter.com/mazaanaqvi",
+    url: "https://twitter.com/asadwaqas",
     icon: "fab fa-twitter",
     cssClass: "twitter",
   },
   {
     name: "GitHub",
-    url: "https://github.com/mazanaqvi",
+    url: "https://github.com/asadwaqas-dev21",
     icon: "fab fa-github",
     cssClass: "github",
   },
   {
     name: "YouTube",
-    url: "https://www.youtube.com/channel/UCfqP1OsD9GJE7BoeZV53AsA",
+    url: "https://www.youtube.com/@computerlearning3134",
     icon: "fab fa-youtube",
     cssClass: "youtube",
   },
   {
     name: "Stack Overflow",
-    url: "https://stackoverflow.com/users/13111347/ali-hamza",
+    url: "https://stackoverflow.com/users/23524200/asad-waqas",
     icon: "fab fa-stack-overflow",
     cssClass: "stackoverflow",
   },
@@ -62,7 +62,7 @@ export const contactSocialLinks: SocialLink[] = [
 export const contactInfo = {
   email: "asadwaqas.dev@gmail.com",
   phone: "+92 302 8207226",
-  phoneHref: "+923333839512",
+  phoneHref: "+923274418427",
   location: "Lahore, Pakistan",
-  linkedin: "https://www.linkedin.com/in/mazanaqvi/",
+  linkedin: "https://www.linkedin.com/in/asad-waqas-87268a34a/",
 };
