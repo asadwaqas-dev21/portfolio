42af25f10f5fcc9b5bbe9b22c63a668728c33c09
asadwaqas-dev21 <asadkambo2021@gmail.com>
2026-02-26 12:57:55 +0500

    vite changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-  },
 })
